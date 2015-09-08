f851d2208a727744a5604a36cc8899b2e053dc45
Karin <k.n.wiecha@gmail.com>
2015-09-08 09:47:20 +0200

    Age function auch bei der intro entfernt

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -55,9 +55,6 @@ var items  = [
 
     ["intro", "Form", {
         html: { include: "example_intro.html" },
-        validators: {
-            age: function (s) { if (s.match(/^\d+$/)) return true; else return "Bad value for \u2018age\u2019"; }
-        }
     } ],
 
     ["demographics", "Form", {
